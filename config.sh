b9188a78cb08221c6795bcb07194e2b988d0be9a
Diggen85 <benny_stark@live.de>
2020-01-06 20:45:31 +0100

    foreign conf handling to postinst
    - replaced config-package-dev displace with hide
    - remove customize.sh, add default.conf under /usr/share
    - add foreign conf as etc.templates
    - modify and install at postinst

--- config.sh
+++ config.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# Masks are fixed /24 for IPv4 and /64 for IPv6
-IPv6HOST=fd00:24::1
-IPv6NET=fd00:24::
-IPv4HOST=192.168.24.1
-IPv4NET=192.168.24.0
-DHCPv4START=192.168.24.201
-DHCPv4END=192.168.24.251
-SSID=raspion
-
 #Builds Version Strings
 MAJOR=1
 MINOR=1
