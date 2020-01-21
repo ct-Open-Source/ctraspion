9d8c71ea741ab5e200d533c50c0737559d0c2a55
Diggen85 <benny_stark@live.de>
2020-01-21 20:42:03 +0100

    prepare release files to be compatible with dev
    - remove all cmds which are now in raspion.deb
    - added passwd at the end

--- release/install.sh
+++ release/install.sh
@@ -18,10 +18,6 @@
 
 set -e
 
-#
-${$RELEASE:=TRUE}
-#Replace Debs / add local repository when FALSE
-
 WD=$(pwd)
 LOG=./raspion-inst.log
 sudo touch $LOG
@@ -46,54 +42,68 @@ sudo apt-get -y --allow-downgrades dist-upgrade >> $LOG 2>&1
 
 echo "* Raspbian Sprachanpassungen ..." | tee -a $LOG
 sudo debconf-set-selections debconf/keyboard-configuration >> $LOG 2>&1
-sudo cp files/keyboard /etc/default >> $LOG 2>&1
+sudo cp debconf/keyboard /etc/default >> $LOG 2>&1
 sudo dpkg-reconfigure -fnoninteractive keyboard-configuration >> $LOG 2>&1
 
 NEWLANG=de_DE.UTF-8
-sudo cp files/locale.gen /etc >> $LOG 2>&1
+sudo cp debconf/locale.gen /etc >> $LOG 2>&1
 sudo dpkg-reconfigure -fnoninteractive locales >> $LOG 2>&1
 sudo update-locale LANG=$NEWLANG >> $LOG 2>&1
 
 sudo debconf-set-selections debconf/tzdata >> $LOG 2>&1
 sudo ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime >> $LOG 2>&1
-sudo cp files/timezone /etc >> $LOG 2>&1
+sudo cp debconf/timezone /etc >> $LOG 2>&1
 sudo dpkg-reconfigure -fnoninteractive tzdata >> $LOG 2>&1
-
 
 echo "* Pakete vorkonfigurieren ..." | tee -a $LOG
 sudo debconf-set-selections debconf/wireshark >> $LOG 2>&1
 sudo debconf-set-selections debconf/iptables-persistent >> $LOG 2>&1
 
+echo "* Bugfix:iptables-1.8.2-4: Aktiviere iptables-legacy" >> $LOG 2>&1
+# iptables-1.8.2-4 has Problems with MASQUERADE and ipv6
+# we are using old iptables anyway
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy >> $LOG 2>&1
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy >> $LOG 2>&1
+
+echo "* Pakete installieren ..." | tee -a $LOG
+sudo apt-get install -y --allow-downgrades raspion --no-install-recommends >> $LOG 2>&1
+
+echo "* Lade raspion's default.conf" | tee -a $LOG
+. /usr/share/raspion/etc.templates/default.conf
 echo "* Pi-hole installieren ..." | tee -a $LOG
 if ! id pihole >/dev/null 2>&1; then
     sudo adduser --no-create-home --disabled-login --disabled-password --shell /usr/sbin/nologin --gecos "" pihole >> $LOG 2>&1
 fi
 sudo mkdir -p /etc/pihole >> $LOG 2>&1
 sudo chown pihole:pihole /etc/pihole >> $LOG 2>&1
-sudo cp $WD/files/setupVars.conf /etc/pihole >> $LOG 2>&1
-sudo sed -i "s/IPV4_ADDRESS=#IPv4HOST#/IPV4_ADDRESS=$IPv4HOST/" /etc/pihole/setupVars.conf >> $LOG 2>&1
-sudo sed -i "s/IPV6_ADDRESS=#IPv6HOST#/IPV6_ADDRESS=$IPv6HOST/" /etc/pihole/setupVars.conf >> $LOG 2>&1
-sudo sed -i "s/DHCP_ROUTER=#IPv4HOST#/DHCP_ROUTER=$IPv4HOST/" /etc/pihole/setupVars.conf >> $LOG 2>&1
-sudo sed -i "s/DHCP_START=#DHCPv4START#/DHCP_START=$DHCPv4START/" /etc/pihole/setupVars.conf >> $LOG 2>&1
-sudo sed -i "s/DHCP_END=#DHCPv4END#/DHCP_END=$DHCPv4END/" /etc/pihole/setupVars.conf >> $LOG 2>&1
+sudo cp $WD/release/pihole/setupVars.conf /etc/pihole >> $LOG 2>&1
+sudo sed -i "s/IPV4_ADDRESS=#IPV4HOST#/IPV4_ADDRESS4HOST#/IPV4_ADDRESS=$IPV4HOST/" /etc/pihole/setupVars.conf >> $LOG 2>&1
+sudo sed -i "s/IPV6_ADDRESS=#IPV6HOST#/IPV6_ADDRESS=$IPV6HOST/" /etc/pihole/setupVars.conf >> $LOG 2>&1
+sudo sed -i "s/DHCP_ROUTER=#IPV4HOST#/DHCP_ROUTER=$IPV4HOST/" /etc/pihole/setupVars.conf >> $LOG 2>&1
+sudo sed -i "s/DHCP_START=#DHCPV4START#/DHCP_START=$DHCPV4START/" /etc/pihole/setupVars.conf >> $LOG 2>&1
+sudo sed -i "s/DHCP_END=#DHCPV4END#/DHCP_END=$DHCPV4END/" /etc/pihole/setupVars.conf >> $LOG 2>&1
 sudo -s <<HERE
 curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended >> $LOG 2>&1
 HERE
 sudo chattr -f -i /etc/init.d/pihole-FTL >> $LOG 2>&1
-sudo cp $WD/files/pihole-FTL /etc/init.d/ >> $LOG 2>&1
+sudo cp $WD/release/pihole/pihole-FTL /etc/init.d/ >> $LOG 2>&1
 sudo chattr -f +i /etc/init.d/pihole-FTL >> $LOG 2>&1
 sudo systemctl daemon-reload >> $LOG 2>&1
 sudo systemctl restart pihole-FTL >> $LOG 2>&1
 sudo pihole -f restartdns >> $LOG 2>&1
 sudo cp $WD/files/hosts /etc/ >> $LOG 2>&1
 
-
+#im Log nach dem hostapd Passwort suchen
+WPAPW=$(cat $LOG | grep "The hostapd Password is: "| cut -d":" -f2)
 
 echo "==> Installation des c't-Raspion erfolgreich abgeschlossen." | tee -a $LOG
 echo ""
 echo "Das Passwort für das WLAN zur Beobachtung lautet: $WPAPW"
-echo "Notieren Sie dieses bitte, ändern Sie auch gleich das Passwort"
-echo "für den Benutzer pi (mit passwd)."
+echo "Notieren Sie dieses bitte!"
+echo ""
+echo "Ändern Sie auch gleich das Passwort"
+echo "für den Benutzer pi."
+passwd
 echo ""
 echo "Starten Sie Ihren Raspberry Pi jetzt neu: sudo reboot now"
 
